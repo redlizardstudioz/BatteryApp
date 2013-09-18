36e62debd26bf2ca8c223c97a9f21a849ac1a215
inder hayer <inderhayer4u@gmail.com>
2013-09-18 15:57:05 +0530

    cloud file updated

--- BatteryApp/cloud/main.js
+++ BatteryApp/cloud/main.js
@@ -1,6 +1,22 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.define("averageBatteryLevel", function(request, response) {
+  var query = new Parse.Query("b352944059607484");
+	query.limit(1000);
+  query.find({
+    success: function(results) {
+      var sum = 0;
+      var total = new Array();
+      for (var i = 0; i < results.length; ++i) {	
+      var abc = results.length;
+        sum += results[i].get("batteryLevel");
+		total[i] = results[i].get("batteryLevel");
+      }
+      response.success(sum/results.length);
+    },
+    error: function() {
+      response.error("Error");
+    }
+  });
 });
